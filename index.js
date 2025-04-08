81d143b1a0e6ea5e6b72fc09c4f50b1f6a4633ec
MPardal <miguelpardal.esparis@ext.essilor.com>
2025-04-08 17:22:19 +0200

    start project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world!!')
+const express = require('express');
+
+//? Create express server
+const app = express();
+
+//* Routes
+app.get('/', (req, res) => {
+    console.log('route')
+    res.json({
+        ok: true
+    });
+});
+
+//? Listen
+app.listen(4000, () => {
+    console.log(`Server running on port ${4000}`)
+});
